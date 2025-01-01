98d96bfff908d2b3a03a62d61f28da91cf0d084a
Vessa-is <islami.vessa@gmail.com>
2025-01-01 22:32:24 +0100

    rregullimi i loop te slides

--- javascript.js
+++ javascript.js
@@ -22,6 +22,19 @@ function  initializeSlider (){
 }
 
 function showSlide (index) {
+
+if(index >= slides.length) {
+    
+    slideIndex=0;
+
+}
+
+else if (index <0 ){
+    slideIndex = slides.length -1 ;
+    
+
+}
+
     slides.forEach (
        slide => {slide.classList.remove ("displaySlide")});
        slides[slideIndex].classList.add("displaySlide");
