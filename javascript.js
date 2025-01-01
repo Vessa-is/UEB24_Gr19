dafca73ac1064bdbcf06e745854813f739ba34e6
Vessa-is <islami.vessa@gmail.com>
2025-01-01 22:23:21 +0100

    funksionet ne javasript per slider images

---
+++ javascript.js
@@ -0,0 +1,39 @@
+//Image Slider 
+
+
+const slides = document.querySelectorAll (".slides img");
+let slideIndex = 0;
+let intervalId = null; 
+
+//Initialize slider
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+
+function  initializeSlider (){
+    
+    if( slides.length > 0) {
+
+        slides [slideIndex].classList.add("displaySlide");
+ intervalId = setInterval( nextSlide, 5000 );
+
+    }
+
+}
+
+function showSlide (index) {
+    slides.forEach (
+       slide => {slide.classList.remove ("displaySlide")});
+       slides[slideIndex].classList.add("displaySlide");
+
+
+}
+
+function prevSlide () {
+
+}
+
+function nextSlide () {
+slideIndex++;
+showSlide(slideIndex);
+}
