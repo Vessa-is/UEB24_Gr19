c73f1ec9f9ffcc2e1e5f66347989ce36aacee4b8
lizesyla <155230620+lizesyla@users.noreply.github.com>
2025-04-08 15:17:33 +0200

    oop

--- script/classes/Product.php
+++ script/classes/Product.php
@@ -1,6 +1,57 @@
 <?php
 class Product{
+    private $id;
+    private $imgurl;
+    private $name;
+    private $price;
+    private $stock;
     
+    function __construct($id, $imgurl, $name, $price, $stock){
+        $this->id = $id;
+        $this->imgurl = $imgurl;
+        $this->name = $name;
+        $this->price = $price;
+        $this->stock = $stock;
+    }
+    
+    function getId(){
+        return $this->id;
+    }
+
+    public function setImgUrl($imgurl){
+        $this->imgurl = $imgurl;
+    }
+
+    function getImgUrl(){
+        return $this->imgurl;
+    }
+
+    public function setName($name){
+        $this->name = $name;
+    }
+
+    function getName(){
+        return $this->name;
+    }
+
+    public function setPrice($price){
+        $this->price = $price;
+    }
+
+    function getPrice(){
+        return $this->price;
+    }
+
+    public function setStock($stock){
+        $this->stock = $stock;
+    }
+
+    function getStock(){
+        return $this->stock;
+    }
+
+
+
 }
 
 ?>
