25858867326369c3fe4c7848525ef40bb682da89
Enesiktm <enesispahiu@gmail.com>
2025-05-24 13:24:08 +0200

    DatabaseConnection update

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -1,26 +1,25 @@
 <?php
 
-class DatabaseConnection{
-    private $server="localhost";
-    private $username="root";
-    private $password="";
-    private $database = "radiant";
+class DatabaseConnection {
+    private $server = "metro.proxy.rlwy.net";
+    private $port = "42104";
+    private $username = "postgres";
+    private $password = "hYvQCxIaVYdbTJJdqhPWdWfJbjVxaSTe";
+    private $database = "railway";
 
-
-    function startConnection(){
-
-        try{
-            $conn = new PDO("mysql:host=$this->server;dbname=$this->database",$this->username);
+    public function startConnection() {
+        try {
+            $conn = new PDO("pgsql:host=$this->server;port=$this->port;dbname=$this->database", $this->username, $this->password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
-        }catch(PDOException $e){
-            echo "Database Connection Failed".$e->getMessage();
+        } catch (PDOException $e) {
+            echo "Database Connection Failed: " . $e->getMessage();
             return null;
         }
     }
 }
 
 $db = new DatabaseConnection();
-$db->startConnection();
+$conn = $db->startConnection();
 
 ?>
