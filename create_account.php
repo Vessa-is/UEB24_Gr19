f6e04d98b02c292324c3440595e5161b310333c6
lizesyla <155230620+lizesyla@users.noreply.github.com>
2025-05-25 14:28:10 +0200

    Update create_account.php

--- create_account.php
+++ create_account.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $formData = [
     'first-name' => '',
     'last-name' => '',
@@ -9,10 +11,8 @@ $formData = [
     'nr-personal' => ''
 ];
 
-
 include 'script/classes/UserRepository.php';
 require_once 'script/classes/User.php';
-
 
 $errors = [];
 
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (empty($formData['data-e-lindjes'])) {
         $errors['data-e-lindjes'] = 'Birthdate is required';
     } elseif (!preg_match('/^\d{2}\-\d{2}\-\d{4}$/', $formData['data-e-lindjes'])) {
-        $errors['data-e-lindjes'] = 'Birthdate should be in DD/MM/YYYY format';
+        $errors['data-e-lindjes'] = 'Birthdate should be in DD-MM-YYYY format';
     }
 
     if (empty($formData['nr-personal'])) {
@@ -71,138 +71,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $errors['nr-personal'] = 'Personal number should be 10 digits';
     }
 
-         // Nese nuk ka errora
-  if (empty($errors)) {
-        $userRepository = new UserRepository();$personalnr = $formData['nr-personal'];
-$email = $formData['email'];
-$name = $formData['first-name'];
-$lastname = $formData['last-name'];
-$hashedPassword = password_hash($formData['password'], PASSWORD_DEFAULT); // gjithmonë hash password!
+    if (empty($errors)) {
+        $userRepository = new UserRepository();
+        $personalnr = $formData['nr-personal'];
+        $email = $formData['email'];
+        $name = $formData['first-name'];
+        $lastname = $formData['last-name'];
+        $hashedPassword = password_hash($formData['password'], PASSWORD_DEFAULT);
 
-if ($userRepository->personalNrExists($personalnr)) {
-    echo "<script>alert('Ky numër personal ekziston tashmë!');</script>";
-} elseif ($userRepository->userExistsByEmail($email)) {
-    echo "<script>alert('Ky email është përdorur tashmë!');</script>";
-} else {
-    $user = new User($name, $lastname, $email, $hashedPassword, $personalnr);
-    $userRepository->insertUser($user);
-    header("Location: login.php");
-    exit();
-}
+        if ($userRepository->personalNrExists($personalnr)) {
+            echo "<script>alert('Ky numër personal ekziston tashmë!');</script>";
+        } elseif ($userRepository->userExistsByEmail($email)) {
+            echo "<script>alert('Ky email është përdorur tashmë!');</script>";
+        } else {
+            $user = new User($name, $lastname, $email, $hashedPassword, $personalnr);
+            $userRepository->insertUser($user);
 
-}
+            $_SESSION['user'] = [
+                'emri' => $name,
+                'mbiemri' => $lastname,
+                'email' => $email,
+                'personalNr' => $personalnr
+            ];
+
+            header("Location: index.php"); 
+            exit();
+        }
+    }
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" href="images/logo1.png" />
-    <title>Create-Radiant Touch</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
-
-    <link rel="stylesheet" href="style.css" />
-
-  </head>
-  <body class="create-page">
-    <header>
-      <nav>
-        <div class="logo-cont">
-          <div class="logo">
-            <a href="index.php">
-              <img src="images/logoo2.png" alt="logo" title="Radiant Touch" />
-            </a>
-          </div>
-          <div class="login">
-            <a href="login.php"><button id="loginBtn" aria-label="Login" >
-                <i class="fa fa-user"></i>
-              </button></a>
-          </div>
-        </div>
-        <div id="navi">
-          <ul>
-            <li><a href="index.php">Ballina</a></li>
-            <li><a href="sherbimet.php">Shërbimet</a></li>
-            <li><a href="galeria.php">Galeria</a></li>
-            <li><a href="Produktet.php">Produktet</a></li>
-            <li><a href="per_ne.php">Rreth nesh</a></li>
-            <li><a href="kontakti.php">Kontakti</a></li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-    <div class="register-container"">
-        <h1 class="register-title">Create Account</h1>
-        <form id="create-account-form" method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-            <label for="first-name" class="form-label">First Name</label>
-            <input class="form-input" type="text" id="first-name" name="first-name" placeholder="First Name" value="<?php echo $formData['first-name']; ?>">
-            <?php if (isset($errors['first-name'])): ?>
-              <span class="error-message"><?php echo $errors['first-name']; ?></span>
-            <?php endif; ?>
-            
-    
-            <label for="last-name" class="form-label">Last Name</label>
-            <input type="text" id="last-name" class="form-input" name="last-name" placeholder="Last Name" value="<?php echo $formData['last-name']; ?>">
-            <?php if (isset($errors['last-name'])): ?>
-              <span class="error-message"><?php echo $errors['last-name']; ?></span>
-            <?php endif; ?>
-
-            <label for="email" class="form-label">Email</label>
-            <input type="email" id="email" class="form-input" name="email" placeholder="Email" value="<?php echo $formData['email']; ?>">
-            <?php if (isset($errors['email'])): ?>
-              <span class="error-message"><?php echo $errors['email']; ?></span>
-            <?php endif; ?>
-            
-    
-            <label for="password" class="form-label">Password</label>
-            <input type="password" id="password" class="form-input" name="password" placeholder="Password" value="<?php echo $formData['password']; ?>">
-            <?php if (isset($errors['password'])): ?>
-              <span class="error-message"><?php echo $errors['password']; ?></span>
-            <?php endif; ?>
-
-            <label for="confirm-password" class="form-label">Confirm Password</label>
-            <input type="password" id="confirm-password" class="form-input" name="confirm-password" placeholder="Confirm password" value="<?php echo $formData['confirm-password']; ?>">
-            <?php if (isset($errors['confirm-password'])): ?>
-              <span class="error-message"><?php echo $errors['confirm-password']; ?></span>
-            <?php endif; ?>
-
-            <label for="data-e-lindjes" class="form-label">Birthdate</label>
-            <input type="text" id="data-e-lindjes" class="form-input" name="data-e-lindjes" placeholder="Birthdate" value="<?php echo $formData['data-e-lindjes']; ?>">
-            <?php if (isset($errors['data-e-lindjes'])): ?>
-              <span class="error-message"><?php echo $errors['data-e-lindjes']; ?></span>
-            <?php endif; ?>
-
-            <label for="nr-personal" class="form-label">Numri personal</label>
-            <input type="text" id="nr-personal" class="form-input" name="nr-personal" placeholder="nr-personal" value="<?php echo $formData['nr-personal']; ?>">
-            <?php if (isset($errors['nr-personal'])): ?>
-              <span class="error-message"><?php echo $errors['nr-personal']; ?></span>
-            <?php endif; ?>
-            
-    
-            <button type="submit" name="signupbutton" class="submit-button">Create New Account</button>
-        </form>
-        <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && empty($errors)) {
-    include_once '../UEB24_Gr19/script/classes/User.php';
-    include_once '../UEB24_Gr19/script/classes/UserRepository.php';
-
-    $firstname = $_POST['first-name'];
-    $lastname = $_POST['last-name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $personalnr = $_POST['nr-personal'];
-
-    $user = new User($firstname, $lastname, $email, $password, $personalnr);
-    $userRepo = new UserRepository();
-    $userRepo->insertUser($user);
-
-  
-    exit();
-}
-
-?>
         <div class="back-link">
             <a href="login.php">Return to Login</a>
         </div>
